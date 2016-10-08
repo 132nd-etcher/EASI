614bcc337711d748a6d7d5de0208c44cfe52a261
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-08 21:39:29 +0200

    fixed epoch test being too fast

--- tests/test_epoch.py
+++ tests/test_epoch.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 
+import time
 from unittest import TestCase
 from src.low.epoch import Epoch
 
@@ -8,6 +9,7 @@ class TestEpoch(TestCase):
 
     def test_online_epoch(self):
         cached_epoch = Epoch.epoch_cached_online()
+        time.sleep(1)
         epoch = Epoch.epoch_online()
         self.assertTrue(epoch > cached_epoch)
         epoch = Epoch.epoch_cached_online()
