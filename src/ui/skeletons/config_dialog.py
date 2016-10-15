b436b8f8873594a50b7c0cbcf2bbad208d4b62a7
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-15 23:20:09 +0200

    config_dialog: show correct tab at start

--- src/ui/skeletons/config_dialog.py
+++ src/ui/skeletons/config_dialog.py
@@ -426,7 +426,7 @@ class Ui_Settings(object):
         self.verticalLayout.addWidget(self.buttonBox)
 
         self.retranslateUi(Settings)
-        self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(Settings)
 
     def retranslateUi(self, Settings):
