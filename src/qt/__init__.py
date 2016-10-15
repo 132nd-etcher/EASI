3323b4daee29c03b23d73728d4570d8b3ec8f5f8
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-15 23:16:58 +0200

    added some more qt imports

--- src/qt/__init__.py
+++ src/qt/__init__.py
@@ -2,11 +2,13 @@
 
 # noinspection PyUnresolvedReferences
 from PyQt5.QtCore import Qt, QThread, QObject, pyqtSignal, pyqtSlot, QAbstractItemModel, QAbstractListModel, QVariant, \
-    QEvent, QPoint, QRect
+    QEvent, QPoint, QRect, QLine
 # noinspection PyUnresolvedReferences
-from PyQt5.QtGui import QIcon, QPixmap, QFont, QBrush
+from PyQt5.QtGui import QIcon, QPixmap, QFont, QBrush, QPainter, QPen
 # noinspection PyUnresolvedReferences
 from PyQt5.QtWidgets import QMainWindow, QSplashScreen, QLabel, QProgressBar, QDialog, QApplication, QStyleFactory, \
-    QComboBox, QAction, QMenu, QFileDialog, QDialogButtonBox, QLineEdit, QToolTip, QToolButton, QWidget
+    QComboBox, QAction, QMenu, QFileDialog, QDialogButtonBox, QLineEdit, QToolTip, QToolButton, QWidget, \
+    QGraphicsDropShadowEffect, QStylePainter
+
 
 from . import qt_resources
