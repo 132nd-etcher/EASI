e922d8e3fb020cee107293ef0d67d7a631952a63
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-16 02:54:46 +0200

    code inspection run

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,10 @@
 # coding=utf-8
 
 import sys
+
 from src import main
-import shutil
 
 
-# @unittest.skipUnless(os.getenv('TEST_MAIN_LOOP') is not None, 'Not testing main loop')
 def test_and_exit():
     sys.argv.append('test_and_exit')
     main.main()
--- tests/test_ui/conftest.py
+++ tests/test_ui/conftest.py
@@ -18,7 +18,7 @@ def mock_main_ui(mocker):
 
 @pytest.fixture()
 def main_ui(qtbot):
-    main_ui = MainUi(None)
-    qtbot.add_widget(main_ui)
-    yield main_ui
-    main_ui.exit()
+    _main_ui = MainUi(None)
+    qtbot.add_widget(_main_ui)
+    yield _main_ui
+    _main_ui.exit()
