63c5758e7366e5751f40612b8bc481ab19383260
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-18 14:08:32 +0200

    removed unused __init__.py

--- src/rem/gh/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-
-# from src.rem.gh.gh_objects.gh_latest_release import GHLatestRelease
-# from .token_request import gh_request_token, TokenRequestError
-# from .token_test import gh_token_is_valid
-
-from .gh_session import GHSessionError, GHAnonymousSession, GHSession
