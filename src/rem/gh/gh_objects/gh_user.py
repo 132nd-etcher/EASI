192e1122164e1d9702fc04d8a7a48a2f0795dad3
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-18 13:51:31 +0200

    added properties to gh_user

--- src/rem/gh/gh_objects/gh_user.py
+++ src/rem/gh/gh_objects/gh_user.py
@@ -20,3 +20,59 @@ class GHUser(BaseGHObject):
     @json_property
     def id(self):
         """"""
+
+    @json_property
+    def avatar_url(self):
+        """"""
+
+    @json_property
+    def url(self):
+        """"""
+
+    @json_property
+    def html_url(self):
+        """"""
+
+    @json_property
+    def repos_url(self):
+        """"""
+
+    @json_property
+    def type(self):
+        """"""
+
+    @json_property
+    def company(self):
+        """"""
+
+    @json_property
+    def blog(self):
+        """"""
+
+    @json_property
+    def location(self):
+        """"""
+
+    @json_property
+    def bio(self):
+        """"""
+
+    @json_property
+    def public_repos(self):
+        """"""
+
+    @json_property
+    def public_gists(self):
+        """"""
+
+    @json_property
+    def created_at(self):
+        """"""
+
+    @json_property
+    def updated_at(self):
+        """"""
+
+    @json_property
+    def email(self):
+        """"""
