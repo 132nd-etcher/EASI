64e59a224afa649a8e335708e525b9b032e8bb2e
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-18 14:09:55 +0200

    REMOVED BUILD FROM __VERSION__
    Was at 11308
    
    Versionning is now handled by GitVersion (or it should be anyway...)

--- src/__version__.py
+++ src/__version__.py
@@ -1,5 +1,14 @@
 # coding=utf-8
 
-__version__ = '0.0.11-alpha.1+11308'
-__build__ = '11308'
+import sys
+import subprocess
+from json import loads
+from src.low.custom_path import Path
+
+if hasattr(sys, 'frozen'):
+    __version__ = Path(sys.executable).get_win32_file_info().file_version
+else:
+    __version__ = loads(subprocess.check_output(['gitversion']).decode().rstrip()).get('FullSemVer')
+
+
 __guid__ = '4ae6dbb7-5b26-43c6-b797-2272f5a074f3'
