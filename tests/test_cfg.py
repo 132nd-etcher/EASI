4adde9d11b8040c58f7cd259aa23f5602a8fa79a
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-18 13:52:20 +0200

    test_cfg is WIP

--- tests/test_cfg.py
+++ tests/test_cfg.py
@@ -2,22 +2,8 @@
 
 from src.cfg.cfg import Config
 from .utils import ContainedTestCase
+import pytest
 
 
-class TestConfig(ContainedTestCase):
-    def __init__(self, *args, **kwargs):
-        super(TestConfig, self).__init__(*args, **kwargs)
-        self.config_path = None
-
-    def setUp(self):
-        super(TestConfig, self).setUp()
-        self.config_path = self.create_temp_file()
-        self.config = Config(config_file=self.config_path)
-
-    # noinspection PyUnresolvedReferences
-    def test_import(self):
-        import src.cfg.values
-        import src.cfg.cfg
-
-        # def test_init(self):
-        #     self.config.debug()
+def test_config_init():
+    pytest.fail()
