5879f7fe28dd885cb070acb2124c64a4ed4933e7
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-20 03:29:59 +0200

    removed unused import

--- src/abstract/ui/__init__.py
+++ src/abstract/ui/__init__.py
@@ -9,7 +9,6 @@ from .base_qobject import AbstractBaseQWidget
 from .connected_dialog import AbstractConnectedDialog
 from .connected_object import AbstractConnectedObject
 from .connected_qobject import AbstractConnectedQObject
-from .long_op import AbstractLongOp
 from .main_ui_interface import AbstractMainUiInterface
 from .main_ui_state import AbstractMainUiState
 from .progress_dialog import BaseProgressDialog
