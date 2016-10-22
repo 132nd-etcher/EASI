20880eca8544a5cd100dd16882b82b35f5a4167a
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-22 23:38:46 +0200

    removed useless mod stub

--- src/mod/meta/__init__.py
+++
@@ -1,4 +0,0 @@
-# coding=utf-8
-
-from .mod_draft_meta import ModDraftMeta
-from .mod_meta import ModMeta
--- src/mod/meta/mod_draft_meta.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-
-from .mod_meta import ModMeta
-
-
-class ModDraftMeta(ModMeta):
-    def __init__(self):
-        ModMeta.__init__(self)
