9555b0e2f5f629a323162164fb0bda100d523608
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-22 23:39:25 +0200

    removed old sig calls

--- src/cfg/values.py
+++ src/cfg/values.py
@@ -5,16 +5,12 @@ import os
 import path
 
 from src.meta.decorators import MetaProperty
-from src.sig import sig_cfg_author_mode, sig_cfg_sg_path, sig_cfg_cache_path, sig_cfg_kdiff_path,\
-    sig_cfg_keyring_encrypt, sig_cfg_active_dcs_install, sig_cfg_user_name, sig_cfg_user_mail,\
-    sig_cfg_subscribe_to_test_versions
 
 
 class ConfigValues:
     @MetaProperty(False, bool)
     def subscribe_to_test_versions(self, value):
         """"""
-        # sig_cfg_subscribe_to_test_versions.value_changed(value)
 
     @MetaProperty(None, str)
     def saved_games_path(self, value: str):
@@ -24,22 +20,18 @@ class ConfigValues:
             raise FileNotFoundError('path does not exist: {}'.format(p.abspath()))
         elif not p.isdir():
             raise TypeError('path is not a directory: {}'.format(p.abspath()))
-        # sig_cfg_sg_path.value_changed(value)
 
     @MetaProperty(None, str)
     def active_dcs_installation(self, value: str):
         """"""
-        # sig_cfg_active_dcs_install.value_changed(value)
 
     @MetaProperty('', str)
     def usr_name(self, value: str):
         """"""
-        # sig_cfg_user_name.value_changed(value)
 
     @MetaProperty('', str)
     def usr_email(self, value: str):
         """"""
-        # sig_cfg_user_mail.value_changed(value)
 
     @MetaProperty(set(), set)
     def ack(self, value: set):
@@ -48,12 +40,10 @@ class ConfigValues:
     @MetaProperty(False, bool)
     def author_mode(self, value: bool):
         """"""
-        # sig_cfg_author_mode.value_changed(value)
 
     @MetaProperty(False, bool)
     def encrypt_keyring(self, value: bool):
         """"""
-        # sig_cfg_keyring_encrypt.value_changed(value)
 
     @MetaProperty(os.path.abspath(r'.\cache'), str)
     def cache_path(self, value: str):
@@ -62,7 +52,6 @@ class ConfigValues:
             os.makedirs(str(p.abspath()))
         elif not p.isdir():
             raise TypeError('there is already a file at: {}'.format(p.abspath()))
-        # sig_cfg_cache_path.value_changed(value)
 
     @MetaProperty(os.path.abspath(r'.\kdiff3\kdiff3.exe'), str)
     def kdiff_path(self, value: str):
@@ -73,4 +62,3 @@ class ConfigValues:
             raise ValueError('not a file: {}'.format(p.abspath()))
         elif not p.name == 'kdiff3.exe':
             raise ValueError('expected "kdiff3.exe", got: {} ({})'.format(p.name, p.abspath()))
-        # sig_cfg_kdiff_path.value_changed(value)
