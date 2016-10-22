dc5c4988ec9434be11a5f6bb1ac19eef117e0ad4
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-22 23:41:30 +0200

    tests now take new entry point into account

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,7 @@
 
 import sys
 
+import pytest
 from blinker_herald import signals
 from pytestqt.qtbot import QtBot
 
@@ -17,8 +18,26 @@ def success(sender, signal_emitter, result):
 def test_and_exit(qtbot: QtBot):
     sys.argv.append('test_and_exit')
     sys.argv.append('no_qt_app')
-    from src import main
-    main.main()
+    from src import easi
+    easi.start_gui()
     sys.argv.pop()
     sys.argv.pop()
     qtbot.wait_until(lambda: test_succeeded is True, timeout=15000)
+    import os
+    del os.environ['REQUESTS_CA_BUNDLE']
+
+
+def test_cert_verify(mocker, tmpdir):
+    import os
+    p = str(tmpdir.join('cacert.pem'))
+    with open(p, 'w') as f:
+        f.write('caribou')
+    with pytest.raises(KeyError):
+        assert os.environ['REQUESTS_CA_BUNDLE'] == ''
+    from src.easi import check_cert
+    check_cert()
+    assert os.environ['REQUESTS_CA_BUNDLE'] is not None
+    assert os.path.exists(os.environ['REQUESTS_CA_BUNDLE'])
+    mocker.patch('certifi.where', return_value=p)
+    with pytest.raises(ImportError):
+        check_cert()
