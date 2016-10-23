7bd98fca441f91bbbe057a405cd96b4f620a219f
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-23 14:11:06 +0200

    removed unused comments

--- src/sig/interface.py
+++ src/sig/interface.py
@@ -5,10 +5,6 @@ from src.sig.base_custom_signal import CustomSignal
 def interfaced_method(func):
     def _wrapper(self, *args, **kwargs):
         return self.send('{}'.format(func.__name__), *args, **kwargs)
-        # try:
-        #     return self.send('{}'.format(func.__name__), *args, **kwargs)
-        # except AttributeError:
-        #     raise AttributeError('unknown function in MainInterface: {}'.format(func.__name__))
 
     return _wrapper
 
