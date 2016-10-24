c1ab5e6be0e6b8e6b98ca6645da41d381e54338b
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-24 09:45:39 +0200

    updated __version__ to use latest beta of gitversion on AV

--- src/__version__.py
+++ src/__version__.py
@@ -1,14 +1,18 @@
 # coding=utf-8
 
-import sys
+import os
 import subprocess
+import sys
 from json import loads
+
 from src.low.custom_path import Path
 
 if hasattr(sys, 'frozen'):
     __version__ = Path(sys.executable).get_win32_file_info().file_version
+elif os.environ.get('APPVEYOR'):
+    __version__ = loads(subprocess.check_output(
+        [r'C:\ProgramData\chocolatey\bin\gitversion.exe']).decode().rstrip()).get('FullSemVer')
 else:
     __version__ = loads(subprocess.check_output(['gitversion']).decode().rstrip()).get('FullSemVer')
-
 
 __guid__ = '4ae6dbb7-5b26-43c6-b797-2272f5a074f3'
