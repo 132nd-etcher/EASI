1f2c9bb3e2d2eb856edd130eea7aa93ba99e3c0e
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-24 10:17:25 +0200

    removed unused imports

--- src/abstract/ui/__init__.py
+++ src/abstract/ui/__init__.py
@@ -1,10 +1,1 @@
 # coding=utf-8
-
-"""
-Holds interfaces for the most basic Qt objects.
-"""
-
-from .base_dialog import AbstractBaseDialog
-from .base_qobject import AbstractBaseQWidget
-from .main_ui_interface import AbstractMainUiInterface
-from .main_ui_state import AbstractMainUiState
