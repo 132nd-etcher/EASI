1a445f5f57340db7ab704ed767d1e45c98e5455d
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-24 15:21:34 +0200

    rewrote input_dialog and added test

--- src/ui/dialog_input/dialog.py
+++ src/ui/dialog_input/dialog.py
@@ -6,36 +6,72 @@ from src.ui.skeletons.input_dialog import Ui_Dialog
 
 # noinspection PyPep8Naming
 class InputDialog(Ui_Dialog, QDialog):
-    def __init__(self, parent):
+    def __init__(self, parent=None):
         QDialog.__init__(self, parent, flags=Qt.WindowSystemMenuHint | Qt.WindowTitleHint | Qt.WindowCloseButtonHint)
+        self.setWindowModality(Qt.ApplicationModal)
         self.setupUi(self)
+        self.questions = {}
 
-    # noinspection PyMethodOverriding
-    @staticmethod
-    def make(text,
-             title=' ',
-             inputs: list = None,
-             ok_btn_text: str = None,
-             cancel_btn_txt: str = None,
-             parent=None) -> dict:
-        if inputs is None:
-            inputs = []
-        output = {}
-        text = text.replace('\n', '<br>')
-        dialog = InputDialog(parent)
-        assert isinstance(inputs, list)
-        for label, default in inputs:
-            output[label] = QLineEdit(default)
-            dialog.formLayout.addRow(QLabel(label), output[label])
-        if ok_btn_text is not None:
-            dialog.buttonBox.button(QDialogButtonBox.Ok).setText(ok_btn_text)
-        if cancel_btn_txt is not None:
-            dialog.buttonBox.button(QDialogButtonBox.Cancel).setText(cancel_btn_txt)
-        dialog.label.setText(text)
-        dialog.setWindowTitle(title)
-        dialog.adjustSize()
-        # base.adjust_size()
-        if dialog.exec() == 1:
-            return {k: output[k].text() for k in output}
-        else:
-            return {}
+    @property
+    def btn_ok(self):
+        return self.buttonBox.button(QDialogButtonBox.Ok)
+
+    @property
+    def btn_cancel(self):
+        return self.buttonBox.button(QDialogButtonBox.Cancel)
+
+    def set_title(self, title: str):
+        self.setWindowTitle(title)
+
+    def set_text(self, text: str):
+        self.label.setText(text.replace('\n', '<br>'))
+
+    def add_question(self, label, default=''):
+        line = QLineEdit()
+        line.setText(default)
+        self.questions[label] = line
+        self.formLayout.addRow(QLabel(label), line)
+
+    def set_btn_ok_text(self, text: str):
+        self.btn_ok.setText(text)
+
+    def set_btn_cancel_text(self, text: str):
+        self.btn_cancel.setText(text)
+
+    def show(self):
+        self.adjustSize()
+        super(InputDialog, self).show()
+
+    @property
+    def result(self):
+        return {k: self.questions[k].text() for k in self.questions}
+
+    # # noinspection PyMethodOverriding
+    # @staticmethod
+    # def make(text,
+    #          title=' ',
+    #          inputs: list = None,
+    #          ok_btn_text: str = None,
+    #          cancel_btn_txt: str = None,
+    #          parent=None) -> dict:
+    #     if inputs is None:
+    #         inputs = []
+    #     dialog = InputDialog(parent)
+    #     dialog.set_title(title)
+    #     dialog.set_text(text)
+    #
+    #     assert isinstance(inputs, list)
+    #     for x in inputs:
+    #         try:
+    #             label, default = x
+    #         except IndexError:
+    #             label, default = x, ''
+    #         dialog.add_question(label, default)
+    #     if ok_btn_text is not None:
+    #         dialog.set_btn_ok_text(ok_btn_text)
+    #     if cancel_btn_txt is not None:
+    #         dialog.set_btn_cancel_text(cancel_btn_txt)
+    #     if dialog.exec() == 1:
+    #         return {k: dialog.questions[k].text() for k in dialog.questions}
+    #     else:
+    #         return {}
---
+++ tests/test_ui/test_input_dialog.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+import random
+import string
+from pytestqt.qtbot import QtBot
+from hypothesis import strategies as st, given, example, settings
+from src.ui.dialog_input.dialog import InputDialog
+from src.qt import Qt
+
+
+@given(title=st.text(), text=st.text(), label1=st.text(), default1=st.text())
+@example(title='', text='', label1='', default1='0')
+@settings(max_examples=20)
+def test_input_dialog_basics(title, text,
+                             label1,
+                             default1,
+                             qtbot: QtBot):
+    dialog = InputDialog()
+    qtbot.add_widget(dialog)
+    dialog.set_title(title)
+    dialog.set_text(text)
+    assert dialog.windowTitle() == title
+    assert dialog.label.text() == text.replace('\n', '<br>')
+    dialog.add_question(label1, default1)
+    dialog.add_question('label2')
+    dialog.show()
+    qtbot.wait_for_window_shown(dialog)
+    qtbot.mouseClick(dialog.btn_ok, Qt.LeftButton)
+    qtbot.wait_signal(dialog.accepted)
+    qtbot.wait_until(lambda: dialog.isVisible() is False)
+    assert dialog.result[label1] == default1
+    assert dialog.result['label2'] == ''
+
+
+@given(label=st.text(min_size=1), default=st.text(), some_text=st.text(min_size=1, max_size=64))
+@settings(max_examples=20)
+def test_input_dialog_results(label, default, some_text, qtbot: QtBot):
+    dialog = InputDialog()
+    qtbot.add_widget(dialog)
+    dialog.add_question(label, default)
+    random_string = ''.join(random.choice(string.ascii_lowercase) for _ in range(32))
+    dialog.questions[label].clear()
+    qtbot.keyClicks(dialog.questions[label], random_string)
+    assert dialog.questions[label].text() == random_string
+    dialog.show()
+    qtbot.wait_for_window_shown(dialog)
+    qtbot.mouseClick(dialog.btn_ok, Qt.LeftButton)
+    qtbot.wait_signal(dialog.accepted)
+    qtbot.wait_until(lambda: dialog.isVisible() is False)
+    qtbot.wait_until(lambda: dialog.questions[label].text() == random_string)
+    assert dialog.result[label] == random_string
+    dialog.questions[label].setText(some_text)
+    dialog.show()
+    qtbot.wait_for_window_shown(dialog)
+    qtbot.mouseClick(dialog.btn_ok, Qt.LeftButton)
+    qtbot.wait_signal(dialog.accepted)
+    qtbot.wait_until(lambda: dialog.isVisible() is False)
+    assert dialog.result[label] == some_text
