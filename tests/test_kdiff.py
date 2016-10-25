7a642834d2b6284d2258d6a95c4c20c68a4088ad
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-25 20:32:40 +0200

    Using tempdir.as_cwd() context instead of custom fixture

--- tests/test_kdiff.py
+++ tests/test_kdiff.py
@@ -10,38 +10,40 @@ from src.helper.kdiff import KdiffHelper
 
 
 class TestKdiff:
-    def test_basics(self, chtmpdir, mocker):
-        with mock.patch(
-                'src.helper.kdiff.KdiffHelper.path',
-                new=mocker.PropertyMock(return_value=Path(str(chtmpdir.mkdir('kdiff').join('kdiff3.exe'))))):
-            kdiff = KdiffHelper()
-            assert kdiff.is_installed is False
-            assert kdiff.name == 'kdiff3'
-            assert kdiff.folder == os.path.dirname(kdiff.path.abspath())
+    def test_basics(self, tmpdir, mocker):
+        with tmpdir.as_cwd():
+            with mock.patch(
+                    'src.helper.kdiff.KdiffHelper.path',
+                    new=mocker.PropertyMock(return_value=Path(str(tmpdir.mkdir('kdiff').join('kdiff3.exe'))))):
+                kdiff = KdiffHelper()
+                assert kdiff.is_installed is False
+                assert kdiff.name == 'kdiff3'
+                assert kdiff.folder == os.path.dirname(kdiff.path.abspath())
 
     @skipUnless(os.getenv('DOLONGTESTS', False) is not False, 'skipping long tests')
-    def test_install(self, mocker: MockFixture, chtmpdir):
-        progress_patch = mock.patch('src.helper.kdiff.SigProgress')
-        progress = progress_patch.start()
-        msg_patch = mock.patch('src.helper.kdiff.SigMsg.show')
-        msg = msg_patch.start()
-        with mock.patch(
-                'src.helper.kdiff.KdiffHelper.path',
-                new=mocker.PropertyMock(return_value=Path(str(chtmpdir.mkdir('kdiff').join('kdiff3.exe'))))):
-            kdiff = KdiffHelper()
-            # assert kdiff.path == Path(str(tmpdir.join('kdiff').join('kdiff3.exe'))).abspath()
-            # return
-            assert kdiff.path.abspath() == Path(str(chtmpdir.join('kdiff').join('kdiff3.exe'))).abspath()
-            assert not os.path.exists(kdiff.path)
-            assert kdiff.is_installed is False
-            kdiff.download_and_install()
-            assert os.path.exists(kdiff.path.abspath())
-            assert kdiff.is_installed is True
-            progress.assert_has_calls(mocker.call('set_progress(0))'), any_order=True)
-            progress.assert_has_calls(mocker.call('set_progress(100))'), any_order=True)
-            progress.assert_has_calls(mocker.call("set_progress_title('Unzipping KDiff3')"), any_order=True)
-            assert msg.call_count == 1
-            msg.assert_has_calls([mock.call('Success', 'KDiff3 has been successfully installed !')], any_order=True)
-        progress_patch.stop()
-        msg_patch.stop()
+    def test_install(self, mocker: MockFixture, tmpdir):
+        with tmpdir.as_cwd():
+            progress_patch = mock.patch('src.helper.kdiff.SigProgress')
+            progress = progress_patch.start()
+            msg_patch = mock.patch('src.helper.kdiff.SigMsg.show')
+            msg = msg_patch.start()
+            with mock.patch(
+                    'src.helper.kdiff.KdiffHelper.path',
+                    new=mocker.PropertyMock(return_value=Path(str(tmpdir.mkdir('kdiff').join('kdiff3.exe'))))):
+                kdiff = KdiffHelper()
+                # assert kdiff.path == Path(str(tmpdir.join('kdiff').join('kdiff3.exe'))).abspath()
+                # return
+                assert kdiff.path.abspath() == Path(str(tmpdir.join('kdiff').join('kdiff3.exe'))).abspath()
+                assert not os.path.exists(kdiff.path)
+                assert kdiff.is_installed is False
+                kdiff.download_and_install()
+                assert os.path.exists(kdiff.path.abspath())
+                assert kdiff.is_installed is True
+                progress.assert_has_calls(mocker.call('set_progress(0))'), any_order=True)
+                progress.assert_has_calls(mocker.call('set_progress(100))'), any_order=True)
+                progress.assert_has_calls(mocker.call("set_progress_title('Unzipping KDiff3')"), any_order=True)
+                assert msg.call_count == 1
+                msg.assert_has_calls([mock.call('Success', 'KDiff3 has been successfully installed !')], any_order=True)
+            progress_patch.stop()
+            msg_patch.stop()
 
