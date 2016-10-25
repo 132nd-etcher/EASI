4dea78271f6f1876a4cd3071069b18ba114a83e7
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-25 20:33:17 +0200

    Added dummy mod for testing

---
+++ tests/mod/dummy1/SAVED GAMES/Liveries/A-10C/74 Sqn/description.lua
@@ -0,0 +1,21 @@
+livery = {
+	{"A-10C_PAINT_1-a", 0 ,"A-10C_RAFAIRUK_74-a",true};
+	{"A-10C_PAINT_1-b", 0 ,"A-10C_RAFAIRUK_74-b",true};
+	{"A-10C_PAINT_1-c", 0 ,"A-10C_RAFAIRUK_74-c",true};
+	{"A-10C_PAINT_1-d", 0 ,"A-10C_RAFAIRUK_74-d",true};
+	{"A-10C_PAINT_1-e", 0 ,"A-10C_RAFAIRUK_74-e",true};
+	{"A-10C_PAINT_1-f", 0 ,"A-10C_RAFAIRUK_74-f",true};
+	{"A-10C_PAINT_1-g", 0 ,"A-10C_RAFAIRUK_74-g",true};
+	{"A-10C_PAINT_1-h", 0 ,"A-10C_RAFAIRUK_74-h",true};
+	{"A-10C_PAINT_1-i", 0 ,"A-10C_RAFAIRUK_74-i",true};
+	{"A-10C_PAINT_1-j", 0 ,"A-10C_RAFAIRUK_74-j",true};
+	{"A-10C_PAINT_1-k", 0 ,"A-10C_RAFAIRUK_74-k",true};
+	{"A-10C_PAINT_1-L", 0 ,"A-10C_RAFAIRUK_74-L",true};
+	{"A-10_Number", 0 ,"empty",true};
+	{"A-10_Number_Noze_F", 0 ,"empty",true};
+	{"A-10_Number_Noze_T", 0 ,"empty",true};
+	{"A-10_Number_Wheel", 0 ,"empty",true};
+	
+
+}
+countries = {"GER", "GRG", "NOR", "RUS", "UKR", "USA", "TUR", "UK", "NETH", "BEL", "DEN"}
