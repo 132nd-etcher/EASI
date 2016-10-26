7c3dafd1e78854c11c1ea0745d9cca6aaf067aec
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-26 15:52:38 +0200

    re-added missing qt imports

--- src/qt/__init__.py
+++ src/qt/__init__.py
@@ -2,13 +2,12 @@
 
 # noinspection PyUnresolvedReferences
 from PyQt5.QtCore import Qt, QThread, QObject, pyqtSignal, pyqtSlot, QAbstractItemModel, QAbstractListModel, QVariant, \
-    QEvent, QPoint, QRect, QLine
+    QEvent, QPoint, QRect, QLine, QRegExp
 # noinspection PyUnresolvedReferences
-from PyQt5.QtGui import QIcon, QPixmap, QFont, QBrush, QPainter, QPen
+from PyQt5.QtGui import QIcon, QPixmap, QFont, QBrush, QPainter, QPen, QRegExpValidator
 # noinspection PyUnresolvedReferences
 from PyQt5.QtWidgets import QMainWindow, QSplashScreen, QLabel, QProgressBar, QDialog, QApplication, QStyleFactory, \
     QComboBox, QAction, QMenu, QFileDialog, QDialogButtonBox, QLineEdit, QToolTip, QToolButton, QWidget, \
-    QGraphicsDropShadowEffect, QStylePainter, QFormLayout, QTextBrowser
-
+    QGraphicsDropShadowEffect, QStylePainter, QFormLayout, QTextBrowser, QWizardPage, QVBoxLayout
 
 from . import qt_resources
