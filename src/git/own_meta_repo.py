2ce1d08aaa767f82a1f485283382d368277af004
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-28 06:44:05 +0200

    added OwnMetaRepo git Repository object

---
+++ src/git/own_meta_repo.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+
+from src.cache.cache import Cache
+from src.git.wrapper import Repository
+from src.low.custom_logging import make_logger
+from src.low.singleton import Singleton
+
+logger = make_logger()
+
+
+class OwnMetaRepo(Repository, metaclass=Singleton):
+    def __init__(self):
+        logger.debug('instantiating')
+        Repository.__init__(self, path=Cache().own_meta_repo_folder)
