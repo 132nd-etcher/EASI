3d13edbb2bb66f20a4fc31bba866bf181983d843
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-28 05:56:38 +0200

    forgot his one for SrcModFile test

--- tests/mod/conftest.py
+++ tests/mod/conftest.py
@@ -8,3 +8,10 @@ import pytest
 def valid_destination(request, tmpdir):
     yield request.param
     tmpdir.remove()
+
+
+# noinspection SpellCheckingInspection
+@pytest.fixture(params=['SGG', 's_g', 'saveddgames', 'DCSD', 'ddcs'])
+def invalid_destination(request, tmpdir):
+    yield request.param
+    tmpdir.remove()
