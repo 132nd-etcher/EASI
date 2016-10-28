ab84b8ed5c0f7518b1249c5052d114f2aac4f260
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-28 05:58:16 +0200

    finished gh_login dialog

--- src/ui/dialog_gh_login/dialog.py
+++ src/ui/dialog_gh_login/dialog.py
@@ -1,49 +1,48 @@
 # coding=utf-8
 
-from src.low import constants
 from src.qt import QDialog, dialog_default_flags, qt_resources, QIcon
+from src.rem.gh.gh_session import GHSession
 from src.ui.base.qdialog import BaseDialog
 from src.ui.form_gh_login.form import GHLoginForm
 from src.ui.skeletons.dialog_gh_login import Ui_Dialog
 
 
 class _GHLoginDialog(QDialog, Ui_Dialog):
-
     def __init__(self, parent=None):
-        if parent is None:
-            parent = constants.MAIN_UI
         QDialog.__init__(self, parent=parent, flags=dialog_default_flags)
         self.setupUi(self)
         self.btn_exit.clicked.connect(self.close)
         self.setWindowTitle('Github login')
         self.setWindowIcon(QIcon(qt_resources.app_ico))
         layout = self.layout()
-        self.forms = {
-            'github': GHLoginForm(self, self.btn_exit)
-        }
-        layout.insertWidget(0, self.forms['github'])
+        self.form = GHLoginForm(self, self.btn_exit)
+
+        layout.insertWidget(0, self.form)
+        self.form.githubUsernameLineEdit.setFocus()
 
     def setup(self):
-        for form in self.forms.values():
-            form.setup()
+        self.form.setup()
 
+    def close(self):
+        super(_GHLoginDialog, self).close()
+
+    def show(self):
+        self.form.show()
+        super(_GHLoginDialog, self).show()
+
+    def exec(self):
+        super(_GHLoginDialog, self).exec()
+        return GHSession().has_valid_token
 
 
 class GHLoginDialog(BaseDialog):
-
     def __init__(self, parent=None):
         BaseDialog.__init__(self, _GHLoginDialog(parent))
         self.qobj.setup()
+        self.qobj.show()
 
-
-
-if __name__ == '__main__':
-    from src.qt import QApplication
-    import sys
-    from src.keyring.keyring import Keyring
-    qt_app = QApplication([])
-    dialog = GHLoginDialog()
-    dialog.qobj.show()
-    from src.rem.gh.gh_session import GHSession
-    GHSession(Keyring().gh_token)
-    sys.exit(qt_app.exec())
+    @staticmethod
+    def make(parent=None):
+        dialog = GHLoginDialog(parent)
+        dialog.qobj.setup()
+        return dialog.qobj.exec()
