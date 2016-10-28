8188b0a2118eb5b5dc63d0e7cb8d4e2bbaa6d97a
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-28 05:48:19 +0200

    added LocalMod class

---
+++ src/mod/local_mod.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+import typing
+from src.cache.cache import Cache
+from src.low.singleton import Singleton
+from src.mod.mod_draft import ModDraft
+
+
+class LocalMod(metaclass=Singleton):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def drafts() -> typing.List[ModDraft]:
+        return [ModDraft(x.basename()) for x in Cache().own_draft_mod_folder.listdir()]
+
+    @staticmethod
+    def mod_name_is_available(name: str, uuid: str) -> bool:
+        for mod_draft in LocalMod.drafts():
+            if mod_draft.name == name and mod_draft.uuid != uuid:
+                return False
+        return True
