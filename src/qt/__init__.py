a19a83609dee2c7bc6780de6c23609ca84bec632
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-29 14:35:31 +0200

    added qt importsadded DCS version to ModDraft

--- src/qt/__init__.py
+++ src/qt/__init__.py
@@ -4,12 +4,13 @@
 from PyQt5.QtCore import Qt, QThread, QObject, pyqtSignal, pyqtSlot, QAbstractItemModel, QAbstractListModel, QVariant, \
     QEvent, QPoint, QRect, QLine, QRegExp, QAbstractTableModel, QModelIndex
 # noinspection PyUnresolvedReferences
-from PyQt5.QtGui import QIcon, QPixmap, QFont, QBrush, QPainter, QPen, QRegExpValidator, QStandardItemModel
+from PyQt5.QtGui import QIcon, QPixmap, QFont, QBrush, QPainter, QPen, QRegExpValidator, QStandardItemModel, QColor,\
+    QStandardItem
 # noinspection PyUnresolvedReferences
 from PyQt5.QtWidgets import QMainWindow, QSplashScreen, QLabel, QProgressBar, QDialog, QApplication, QStyleFactory, \
     QComboBox, QAction, QMenu, QFileDialog, QDialogButtonBox, QLineEdit, QToolTip, QToolButton, QWidget, \
     QGraphicsDropShadowEffect, QStylePainter, QFormLayout, QTextBrowser, QWizardPage, QVBoxLayout, QWizard,\
-    QPushButton, QTableView, QHBoxLayout
+    QPushButton, QTableView, QHBoxLayout, QFileSystemModel, QTreeWidgetItem, QHeaderView
 
 from . import qt_resources
 
