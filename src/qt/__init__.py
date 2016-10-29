dd80a77f5887d4b6a41805c234a5faf685f3f242
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-29 07:16:08 +0200

    added Qt imports

--- src/qt/__init__.py
+++ src/qt/__init__.py
@@ -9,7 +9,7 @@ from PyQt5.QtGui import QIcon, QPixmap, QFont, QBrush, QPainter, QPen, QRegExpVa
 from PyQt5.QtWidgets import QMainWindow, QSplashScreen, QLabel, QProgressBar, QDialog, QApplication, QStyleFactory, \
     QComboBox, QAction, QMenu, QFileDialog, QDialogButtonBox, QLineEdit, QToolTip, QToolButton, QWidget, \
     QGraphicsDropShadowEffect, QStylePainter, QFormLayout, QTextBrowser, QWizardPage, QVBoxLayout, QWizard,\
-    QPushButton, QTableView
+    QPushButton, QTableView, QHBoxLayout
 
 from . import qt_resources
 
