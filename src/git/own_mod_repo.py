7c7cf30b2c222db6c32729fe5131108404c34e02
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-30 06:20:04 +0100

    cleaned up OwnModRepo

--- src/git/own_mod_repo.py
+++ src/git/own_mod_repo.py
@@ -1,17 +1,14 @@
 # coding=utf-8
 
 
-from src.cache.cache import Cache
 from src.git.wrapper import Repository
 from src.low.custom_logging import make_logger
-from src.low.singleton import Singleton
 
 logger = make_logger(__name__)
 
 
-class OwnModRepo(Repository, metaclass=Singleton):
+class OwnModRepo(Repository):
     def __init__(self, path):
-        logger.debug('instantiating')
         Repository.__init__(self, path=path)
         if not self.path.exists():
             self.init()
