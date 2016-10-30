0fc3d8b82d7c173a9dda22bab5ca8a83e907ee49
132nd-etcher <132nd-etcher@daribouca.net>
2016-10-30 06:22:15 +0100

    added BaseMod

---
+++ src/mod/mod_objects/mod_base.py
@@ -0,0 +1,67 @@
+# coding=utf-8
+
+import abc
+
+import semver
+
+from src.cache.cache import Cache
+from src.git.own_mod_repo import OwnModRepo
+from src.meta.meta_property import MetaProperty
+from src.mod.dcs_version import DCSVersion
+
+
+class BaseMod:
+    def __init__(self, uuid):
+        self.uuid = uuid
+        self._repo = None
+
+    @property
+    def repo(self):
+        if self._repo is None:
+            self._repo = OwnModRepo(path=Cache().own_mods_folder.joinpath(self.name))
+        return self._repo
+
+    @abc.abstractclassmethod
+    def trash(self):
+        """"""
+
+    @abc.abstractclassmethod
+    def write(self):
+        """"""
+
+    @property
+    @abc.abstractproperty
+    def status(self):
+        """"""
+
+    @property
+    def has_changed(self):
+        return len(self.repo.status) > 0
+
+    @MetaProperty(None, str)
+    def uuid(self, value: str) -> str:
+        """"""
+
+    @MetaProperty('', str)
+    def name(self, value: str) -> str:
+        """"""
+
+    @MetaProperty(None, str)
+    def category(self, value: str) -> str:
+        """"""
+
+    @MetaProperty('', str)
+    def description(self, value: str) -> str:
+        """"""
+
+    @MetaProperty('', str)
+    def version(self, value: str) -> str:
+        try:
+            semver.parse(value)
+        except ValueError:
+            raise
+
+    @MetaProperty('', str)
+    def dcs_version(self, value: str) -> str:
+        if not DCSVersion.is_valid(value):
+            raise ValueError('not a valid DCS version: {}'.format(value))
--- src/mod/mod_objects/mod_draft.py
+++ src/mod/mod_objects/mod_draft.py
@@ -1,64 +1,26 @@
 # coding=utf-8
 
 
-
-import semver
-
 from src.cache.cache import Cache
-from src.git.own_mod_repo import OwnModRepo
 from src.meta.meta import Meta
-from src.meta.meta_property import MetaProperty
-from src.mod.dcs_version import DCSVersion
+from src.mod.mod_objects.mod_base import BaseMod
+from send2trash import send2trash
 
 
-class ModDraft(Meta):
+class ModDraft(Meta, BaseMod):
+    def trash(self):
+        send2trash(self.path)
+        send2trash(self.repo.path)
+
     @property
     def meta_header(self):
         return 'EASI_MOD_DRAFT'
 
     def __init__(self, uuid):
         Meta.__init__(self, path=Cache().own_mods_folder.joinpath('{}.easi_mod_draft'.format(uuid)))
+        BaseMod.__init__(self, uuid)
         self.uuid = uuid
-        self.__repo = None
-
-    @property
-    def repo(self):
-        if self.__repo is None:
-            self.__repo = OwnModRepo(path=Cache().own_mods_folder.joinpath(self.name))
-        return self.__repo
 
     @property
     def status(self):
         return 'draft'
-
-    @property
-    def has_changed(self):
-        return self.repo.status != 0
-
-    @MetaProperty(None, str)
-    def uuid(self, value: str) -> str:
-        """"""
-
-    @MetaProperty('', str)
-    def name(self, value: str) -> str:
-        """"""
-
-    @MetaProperty(None, str)
-    def category(self, value: str) -> str:
-        """"""
-
-    @MetaProperty('', str)
-    def description(self, value: str) -> str:
-        """"""
-
-    @MetaProperty(None, str)
-    def version(self, value: str) -> str:
-        try:
-            semver.parse(value)
-        except ValueError:
-            raise
-
-    @MetaProperty(None, str)
-    def dcs_version(self, value: str) -> str:
-        if not DCSVersion.is_valid(value):
-            raise ValueError('not a valid DCS version: {}'.format(value))
