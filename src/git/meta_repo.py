cd8b21661153f69547aa36259a3bd3f9032cd9eb
132nd-etcher <132nd-etcher@daribouca.net>
2016-11-02 11:47:56 +0100

    added meta repo object

---
+++ src/git/meta_repo.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+from src.git.wrapper import Repository
+
+
+class MetaRepo(Repository):
+
+    def __init__(self, path):
+        self.__repo = Repository.__init__(self, path, auto_init=False)
+
+    @staticmethod
+    def clone(url, path, bare=False, repository=None, remote=None, checkout_branch='master', callbacks=None):
+        Repository.clone(url, path, bare, repository, remote, checkout_branch, callbacks)
+        return MetaRepo(path)
