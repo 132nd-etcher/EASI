c90d9ca174539cc7dd23037594828e4eaf5f642c
132nd-etcher <132nd-etcher@daribouca.net>
2016-11-02 14:46:14 +0100

    added remote widget to mod view

--- src/ui/dialog_own_mod/dialog.py
+++ src/ui/dialog_own_mod/dialog.py
@@ -4,6 +4,7 @@ from src.qt import QDialog, dialog_default_flags, QWidget
 from src.ui.base.qdialog import BaseDialog
 from src.ui.dialog_own_mod.widget_mod_files import ModFilesWidget
 from src.ui.dialog_own_mod.widget_mod_metadata import ModMetadataWidget
+from src.ui.dialog_own_mod.widget_mod_remote import ModRemoteWidget
 from src.ui.skeletons.form_own_mod_view import Ui_Form
 from src.mod.mod import Mod
 
@@ -12,15 +13,19 @@ class _OwnModDialog(QDialog, Ui_Form):
     def __init__(self, mod: Mod or None, parent=None):
         QDialog.__init__(self, parent, flags=dialog_default_flags)
         self.setupUi(self)
-        self.__mod = None
+        self.__mod = mod
         self.metadata_widget = ModMetadataWidget(self.mod, self)
         self.files_widget = ModFilesWidget(self.mod, self)
+        self.remote_widget = ModRemoteWidget(self.mod, self)
         self.main_layout.addWidget(self.metadata_widget)
         self.main_layout.addWidget(self.files_widget)
+        self.main_layout.addWidget(self.remote_widget)
         self.btn_metadata.setChecked(True)
         self.files_widget.setHidden(True)
+        self.remote_widget.setHidden(True)
         self.btn_metadata.clicked.connect(lambda: self.show_widget(self.metadata_widget))
         self.btn_local_files.clicked.connect(lambda: self.show_widget(self.files_widget))
+        self.btn_remote.clicked.connect(lambda: self.show_widget(self.remote_widget))
         self.mod = mod
 
     @property
@@ -32,14 +37,18 @@ class _OwnModDialog(QDialog, Ui_Form):
         self.__mod = value
         if value is None:
             self.btn_local_files.setEnabled(False)
+            self.btn_remote.setEnabled(False)
         else:
             self.btn_local_files.setEnabled(True)
+            self.btn_remote.setEnabled(True)
             self.metadata_widget.mod = value
             self.files_widget.mod = value
+            self.remote_widget.mod = value
 
     def show_widget(self, widget: QWidget):
         self.metadata_widget.setHidden(True)
         self.files_widget.setHidden(True)
+        self.remote_widget.setHidden(True)
         widget.setHidden(False)
 
 
