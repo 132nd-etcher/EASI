5cecb08a51a670a0c7d3ac640b707d772880781f
132nd-etcher <132nd-etcher@daribouca.net>
2016-11-05 20:18:11 +0100

    added GH sigs

--- src/sig/__init__.py
+++ src/sig/__init__.py
@@ -6,8 +6,11 @@ from .sigmsg import SigMsg
 from .sigprogress import SigProgress
 
 SIG_PROGRESS = signal('SigProgress', doc='App-wide progress update signal; src.abstract.progress_interface')
-SIG_MSG = signal('SigMsg', doc='App-wide signal sent whenever there\'s a need to notify the user about something')
+SIG_MSG = signal('SigMsg', doc='Triggers a msgbox or an laternative way of letting the user know about something')
+
 SIG_LOCAL_MOD_CHANGED = signal('SigLocalModChanged',
                                doc='App-wide signal sent when a local mod is changed, added or deleted')
-SIG_LOCAL_REPO_CHANGED = signal('SigLocalRepoChanged',
-                                doc='App-wide signal sent when a local repo is changed, added or deleted')
+SIG_LOCAL_REPO_CHANGED = signal('SigLocalRepoChanged', doc='')
+
+SIG_CREDENTIALS_GH_CHANGED = signal('SigGHCredentialsChanged', doc='')
+SIG_CREDENTIALS_GH_AUTH_STATUS = signal('SigGHCredentialsAuthStatus', doc='')
