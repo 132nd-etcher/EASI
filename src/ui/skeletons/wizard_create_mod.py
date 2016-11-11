78bbf9f5c86bc78e57c10f090e458c5eb169f2b4
132nd-etcher <132nd-etcher@daribouca.net>
2016-11-11 12:26:46 +0100

    added mod creation wizard

---
+++ src/ui/skeletons/wizard_create_mod.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'F:\DEV\EASI\EASIv0.0.11\src\ui\skeletons\wizard_create_mod.ui'
+#
+# Created by: PyQt5 UI code generator 5.7
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Wizard(object):
+    def setupUi(self, Wizard):
+        Wizard.setObjectName("Wizard")
+        Wizard.setWindowModality(QtCore.Qt.ApplicationModal)
+        Wizard.resize(526, 300)
+        Wizard.setSizeGripEnabled(True)
+        Wizard.setModal(True)
+        Wizard.setWizardStyle(QtWidgets.QWizard.ModernStyle)
+        Wizard.setOptions(QtWidgets.QWizard.DisabledBackButtonOnLastPage|QtWidgets.QWizard.HaveHelpButton|QtWidgets.QWizard.HelpButtonOnRight|QtWidgets.QWizard.NoBackButtonOnStartPage)
+
+        self.retranslateUi(Wizard)
+        QtCore.QMetaObject.connectSlotsByName(Wizard)
+
+    def retranslateUi(self, Wizard):
+        _translate = QtCore.QCoreApplication.translate
+        Wizard.setWindowTitle(_translate("Wizard", "Wizard"))
+
--- src/ui/wizard_create_mod/wizard.py
+++ src/ui/wizard_create_mod/wizard.py
@@ -1,1 +1,35 @@
 # coding=utf-8
+
+
+from src.ui.skeletons.wizard_create_mod import Ui_Wizard
+from src.ui.base.qdialog import BaseDialog
+from src.qt import Qt, QWizard, QWizardPage, dialog_default_flags
+from .page_gh_login import GHLoginPage
+from .page_final import FinalPage
+from src.rem.gh.gh_session import GHSession
+
+
+class _ModCreationWizard(Ui_Wizard, QWizard):
+
+    def __init__(self, parent=None):
+        QWizard.__init__(self, parent, flags=dialog_default_flags)
+        self.setupUi(self)
+        if not GHSession().user:
+            self.addPage(GHLoginPage(self))
+        self.addPage(FinalPage(self))
+
+    def exec(self):
+        if super(_ModCreationWizard, self).exec() == self.Accepted:
+            return {}
+        else:
+            return None
+
+
+class ModCreationWizard(BaseDialog):
+
+    def __init__(self, parent=None):
+        BaseDialog.__init__(self, _ModCreationWizard(parent))
+
+    @staticmethod
+    def make(parent=None):
+        return ModCreationWizard(parent).qobj.exec()
