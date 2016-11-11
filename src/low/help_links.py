15effd413692b0ecc8eb3349faefadd3741a6018
132nd-etcher <132nd-etcher@daribouca.net>
2016-11-11 21:03:56 +0100

    updated help links

--- src/low/help_links.py
+++ src/low/help_links.py
@@ -3,6 +3,6 @@
 mod_creation = r'https://132nd-etcher.github.io/EASI/mod_creation/'
 mod_creation_meta_repo = r'https://132nd-etcher.github.io/EASI/mod_creation/#step-1-repository'
 mod_creation_name = r'https://132nd-etcher.github.io/EASI/mod_creation/#step-2-name'
-mod_creation_type = r'https://132nd-etcher.github.io/EASI/mod_creation/#step-3-mod-category'
+mod_creation_category = r'https://132nd-etcher.github.io/EASI/mod_creation/#step-3-mod-category'
 mod_creation_version = r'https://132nd-etcher.github.io/EASI/mod_creation/#step-4-initial-version-number'
 mod_creation_dcs_version = r'https://132nd-etcher.github.io/EASI/mod_creation/#step-5-dcs-version'
