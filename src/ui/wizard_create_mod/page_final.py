d1752fb5d70647ca236be0dbd51d680f3947f8c6
132nd-etcher <132nd-etcher@daribouca.net>
2016-11-11 12:26:53 +0100

    added mod creation wizard final page

--- src/ui/wizard_create_mod/page_final.py
+++ src/ui/wizard_create_mod/page_final.py
@@ -1,1 +1,13 @@
 # coding=utf-8
+
+from src.qt import QWizardPage, QLabel, QVBoxLayout
+
+
+class FinalPage(QWizardPage):
+
+    def __init__(self, parent=None):
+        QWizardPage.__init__(self, parent)
+        self.setLayout(QVBoxLayout(self))
+        self.label = QLabel('Your mod is ready to be created !\n\n'
+                            'Click "Finish" to begin adding files to it.')
+        self.layout().addWidget(self.label)
