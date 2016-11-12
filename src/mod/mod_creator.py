ded3ef6ddafc0fee9140b29b803af998c35d254d
132nd-etcher <132nd-etcher@daribouca.net>
2016-11-12 11:27:05 +0100

    write mod draft after collection of basics

--- src/mod/mod_creator.py
+++ src/mod/mod_creator.py
@@ -6,6 +6,7 @@ from src.easi.ops import long_input, \
     create_new_mod_collect_basics
 from src.low import constants
 from src.low.custom_logging import make_logger
+from src.meta_repo.local_meta_repo import LocalMetaRepo
 from src.sig import SIG_CREATE_NEW_MOD, SigMsg
 
 logger = make_logger(__name__)
@@ -14,8 +15,19 @@ RE_MOD_NAME = compile(""".*[a-zA-Z]{4,}.*""")
 
 class ModCreator:
     def __init__(self, meta_repo_name=None):
-        self.__basics = create_new_mod_collect_basics(constants.MAIN_UI, meta_repo_name)
-        print(self.__basics)
+        self.basics = create_new_mod_collect_basics(constants.MAIN_UI, meta_repo_name)
+        if self.basics:
+            self.create_draft_mod_object()
+
+    def create_draft_mod_object(self):
+        meta_repo = LocalMetaRepo()[self.basics['meta_repo_name']]
+        mod = meta_repo.create_new_mod(self.basics['mod_name'])
+        mod.meta.category = self.basics['category']
+        mod.meta.version = self.basics['mod_version']
+        mod.meta.dcs_version = self.basics['dcs_version']
+        mod.meta.description = self.basics['mod_desc']
+        mod.meta.status = 'draft'
+        mod.meta.write()
 
     def _get_desc(self):
         desc = long_input(
