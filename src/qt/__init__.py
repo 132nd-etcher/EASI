3e071adff8e8d07505e13876c1f9c8345985e9ee
132nd-etcher <132nd-etcher@daribouca.net>
2016-11-13 16:32:56 +0100

    added qt imports

--- src/qt/__init__.py
+++ src/qt/__init__.py
@@ -2,7 +2,8 @@
 
 # noinspection PyUnresolvedReferences
 from PyQt5.QtCore import Qt, QThread, QObject, pyqtSignal, pyqtSlot, QAbstractItemModel, QAbstractListModel, QVariant, \
-    QEvent, QPoint, QRect, QLine, QRegExp, QAbstractTableModel, QModelIndex, QSortFilterProxyModel, QSize, pyqtProperty
+    QEvent, QPoint, QRect, QLine, QRegExp, QAbstractTableModel, QModelIndex, QSortFilterProxyModel, QSize, pyqtProperty,\
+    QTimer
 # noinspection PyUnresolvedReferences
 from PyQt5.QtGui import QIcon, QPixmap, QFont, QBrush, QPainter, QPen, QRegExpValidator, QStandardItemModel, QColor,\
     QStandardItem
@@ -11,7 +12,7 @@ from PyQt5.QtWidgets import QMainWindow, QSplashScreen, QLabel, QProgressBar, QD
     QComboBox, QAction, QMenu, QFileDialog, QDialogButtonBox, QLineEdit, QToolTip, QToolButton, QWidget, \
     QGraphicsDropShadowEffect, QStylePainter, QFormLayout, QTextBrowser, QWizardPage, QVBoxLayout, QWizard,\
     QPushButton, QTableView, QHBoxLayout, QFileSystemModel, QTreeWidgetItem, QHeaderView, QSpacerItem, QSizePolicy,\
-    QTextEdit
+    QTextEdit, QItemDelegate, QStyleOptionViewItem
 
 from . import qt_resources
 
