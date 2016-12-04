7fd202592f22f8087b4f92acb86bc20cf04aa77f
132nd-etcher <132nd-etcher@daribouca.net>
2016-12-04 19:42:21 +0100

    removed buttons from ui (dynamically added)

--- src/ui/skeletons/form_mod_files_table.py
+++ src/ui/skeletons/form_mod_files_table.py
@@ -20,20 +20,8 @@ class Ui_Form(object):
         self.horizontalLayout.addWidget(self.tableView)
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
-        self.btn_open = QtWidgets.QPushButton(Form)
-        self.btn_open.setObjectName("btn_open")
-        self.verticalLayout.addWidget(self.btn_open)
-        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
-        self.verticalLayout.addItem(spacerItem)
-        self.btn_save = QtWidgets.QPushButton(Form)
-        self.btn_save.setObjectName("btn_save")
-        self.verticalLayout.addWidget(self.btn_save)
-        self.btn_reset = QtWidgets.QPushButton(Form)
-        self.btn_reset.setObjectName("btn_reset")
-        self.verticalLayout.addWidget(self.btn_reset)
-        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItem1)
         self.horizontalLayout.addLayout(self.verticalLayout)
+        self.horizontalLayout.setStretch(0, 1)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -41,7 +29,4 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
-        self.btn_open.setText(_translate("Form", "Open in explorer"))
-        self.btn_save.setText(_translate("Form", "Save"))
-        self.btn_reset.setText(_translate("Form", "Reset"))
 
